a14a27d5ffebc6509f10da13f3e64673d03be59e
Nikola Yordanov <nikolayordanov2005@icloud.com>
2024-01-25 20:23:05 +0200

    final commit

--- src/pages/api/movies.js
+++ src/pages/api/movies.js
@@ -2,14 +2,18 @@
 import { pool } from '../../../db';
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
+  if (req.method === 'GET') {                                           // GET endpoint
     const { rows } = await pool.query('SELECT * FROM movies');
     res.status(200).json(rows);
-  } else if (req.method === 'POST') {
+  } 
+
+  else if (req.method === 'POST') {                                     // POST endpoint
     const { title, genre } = req.body;
     const { rows } = await pool.query('INSERT INTO movies (title, genre) VALUES ($1, $2) RETURNING *', [title, genre]);
     res.status(201).json(rows[0]);
-  } else if (req.method === 'PUT') {
+  } 
+
+  else if (req.method === 'PUT') {                                       // PUT endpoint
     const { id, title, genre } = req.body;
 
     const existingMovie = await pool.query('SELECT * FROM movies WHERE id = $1', [id]);
@@ -20,7 +24,9 @@ export default async function handler(req, res) {
       const { rows } = await pool.query('UPDATE movies SET title = $1, genre = $2 WHERE id = $3 RETURNING *', [title, genre, id]);
       res.status(200).json(rows[0]);
     }
-  } else if (req.method === 'DELETE') {
+  } 
+
+  else if (req.method === 'DELETE') {                                    // DELETE endpoint
     const { id } = req.body;
 
     const existingMovie = await pool.query('SELECT * FROM movies WHERE id = $1', [id]);
@@ -31,7 +37,9 @@ export default async function handler(req, res) {
       await pool.query('DELETE FROM movies WHERE id = $1', [id]);
       res.status(200).end();
     }
-  } else {
+  } 
+  
+  else {
     res.status(405).end();
   }
 }
